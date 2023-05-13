9e32df6ec3502336d98087f6b122b68af986748b
akhilmondal <akhilmandalfamily@gmail.com>
2023-05-13 18:15:39 +0530

    Added: Geter and Setter added to the employee payroll app.

---
+++ Employee.js
@@ -0,0 +1,72 @@
+//UC1_GettersAndSetters
+class EmployeePayrollData {
+    //Getter And Setter Method
+    get id() {
+      return this._id;
+    }
+    set id(id) {
+      this._id = id;
+    }
+    get name() {
+      return this._name;
+    }
+    set name(name) {
+      let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$')
+      if(nameRegex.test(name)) {
+        this._name=name;
+      }else {
+        throw "Name is incorrect"
+      }
+    }
+    get profilePic() {
+      return this._profilePic;
+    }
+    set profilePic (profilePic) {
+      this._profilePic = profilePic;
+    }
+    get gender () {
+      return this._gender;
+    }
+    set gender(gender) {
+      this._gender = gender;
+    }
+    get salary () {
+      return this._salary;
+    }
+    set salary(salary) {
+      this._salary = salary;
+    }
+    get department () {
+      return this._department;
+    }
+    set department(department) {
+      this._department = department
+    }
+    get note () {
+      return this._note;
+    }
+    set note(note) {
+      this._note = note
+    }
+    get startDate () {
+      return this._startDate;
+    }
+    set startDate(startDate) {
+      this._startDate = startDate;
+    }
+  
+    //To String Method
+    toString() {
+      const options = {year: 'numeric', month: 'long', day: 'numeric'};
+      
+    }
+    //method 
+    toString() {
+
+        const options = { year: 'numeric', month: long, day: 'numeric' }; 
+        const empDate = !this.startDate ? "undefined" : this.startDate.toLocaleDateString("en-US", options); 
+        return "id=" +this.id +", names='" + this.name+", gender='" + this.gender+ 
+               ", profilePics='" + this.profilePic+ ", department=" + this.department + 
+               ", salary=" + this.salary +", startDate=" + empDate +", notes=" + this.note;
+    }
+}
